b9e388e22df3ef1e4d6b67dd71ee2ff76d152c64
Janer B M <janerbastos@gmail.com>
2024-03-01 10:02:28 -0300

    Atualidado metodo de inseção usando plocos de renderição parcial

--- web/main.go
+++ web/main.go
@@ -31,9 +31,8 @@ func main() {
 		time.Sleep(1 * time.Second)
 		title := r.PostFormValue("title")
 		director := r.PostFormValue("director")
-		htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white' >%s -%s</li>", title, director)
-		tmpl, _ := template.New("t").Parse(htmlStr)
-		tmpl.Execute(w, nil)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 	}
 
 	http.HandleFunc("/", h1)
